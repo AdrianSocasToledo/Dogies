20fc218e0caa324d30a84065935f465d9b709b8a
AdrianSocasToledo <adrian.socastoledo@gmail.com>
2020-09-05 15:10:08 +0100

    Nuevo titulo

--- src/components/Templates/Header.js
+++ src/components/Templates/Header.js
@@ -1,40 +1,39 @@
-import React,{useState, useEffect} from 'react'
-import './Header.css'
-import { Combobox } from 'react-widgets'
-import getBreeds from '../../services/getBreeds'
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+import { Combobox } from "react-widgets";
+import getBreeds from "../../services/getBreeds";
 
-function Header({onSelectedBreed}){
+function Header({ onSelectedBreed }) {
+  const [breeds, setBreeds] = useState([]);
 
-    const [breeds,setBreeds] = useState([]);
+  useEffect(() => {
+    getBreeds().then((message) =>
+      setBreeds(Object.keys(message).map((breed) => breed))
+    );
+  }, []);
 
-    useEffect( () => {
-        getBreeds().then( message => setBreeds(Object.keys(message).map(breed =>
-            breed)))
-    },[]);
+  const handleOnChange = (value) => {
+    onSelectedBreed(value);
+  };
 
-    const handleOnChange = (value) => {
-        onSelectedBreed(value);
-    }
-
-    return (
+  return (
+    <div>
+      <nav>
+        <div className="logoHeader">Dogies</div>
         <div>
-            <nav>
-                <div className="logoHeader">Galería de imágenes</div>
-                <div><Combobox className="DropDownList"
-                    data={breeds}
-                    defaultValue="boxer"
-                    onChange= {handleOnChange}
-                 />
-                 {
-                     //TODO subrazas.
-                     
-                 }
-                 </div>
-                
-            </nav>
+          <Combobox
+            className="DropDownList"
+            data={breeds}
+            defaultValue="boxer"
+            onChange={handleOnChange}
+          />
+          {
+            //TODO subrazas.
+          }
         </div>
-    )
-
+      </nav>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
